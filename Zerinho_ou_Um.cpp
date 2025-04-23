5e5bcbf4e74029cddb02e6fb451c8c2de5f26dd7
Reinaehfe <reinaldoferro4@gmail.com>
2025-04-23 10:56:13 -0300

    Enviando o código

---
+++ Zerinho_ou_Um.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cin >> a >> b >> c;
+
+	if(a == b && a != c){
+		cout << 'C' << endl;
+	}else if(b == c && b != a){
+		cout << 'A' << endl;
+	}else if(a == c && a != b){
+		cout << 'B' << endl;
+	}else{
+		cout << '*' << endl;
+	}
+}
